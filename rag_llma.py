d36eff944fd76e19818aedf475c94f7a9a7089b7
Priyusha Chinta <priyusha.ch99@gmail.com>
2026-01-20 20:14:43 -0600

    Commit

--- rag_llma.py
+++ rag_llma.py
@@ -7,7 +7,7 @@ import os
 embedder = SentenceTransformer("all-MiniLM-L6-v2")
 
 try:
-    client = Groq(api_key=st.secrets["GROQ_API_KEY"]) # Streamlit Cloud
+    client = Groq(api_key=st.secrets["GROQ"]["API_KEY"]) # Streamlit Cloud
 except Exception:
     client = Groq(api_key=os.getenv("GROQ_API_KEY")) # Local .env
 
--- summarizer.py
+++ summarizer.py
@@ -7,7 +7,7 @@ from cache_utils import get_hash, load_cache, save_cache
 #client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 try:
-    client = Groq(api_key=st.secrets["GROQ_API_KEY"]) # Streamlit Cloud
+    client = Groq(api_key=st.secrets["GROQ"]["API_KEY"]) # Streamlit Cloud
 except Exception:
     client = Groq(api_key=os.getenv("GROQ_API_KEY")) # Local .env
 
